6ad5331309dcca35efb7179c752e2287b4361e04
yarbschris <123770805+yarbschris@users.noreply.github.com>
2026-02-11 14:29:34 -0600

    Add pcnt source and headers, update custom adafruit package

---
+++ src/pcnt/nrf52_pcnt.h
@@ -0,0 +1,26 @@
+#ifndef NRF52_PCNT_H
+#define NRF52_PCNT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <nrfx.h>
+#include <nrfx_gpiote.h>
+#include <nrfx_ppi.h>
+#include <nrfx_timer.h>
+
+// pcntInit: Initialize pulse counter on../../nrf52_pcnt.h../../nrf52_pcnt.h param pin
+nrfx_err_t pcntInit(int pin);
+
+// pcntClear: Resets value of counter to zero
+void pcntClear(void);
+
+// pcntGetCount: Capture current value of counter and return it
+uint32_t pcntGetCount(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // NRF52_PCNT_H
